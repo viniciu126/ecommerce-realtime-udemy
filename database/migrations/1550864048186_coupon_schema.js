d8cd436605da9f7d2cd3f74a4acb93a7c907685e
Vinicius Maciel <viniciu.mn8@hotmail.com>
2019-03-06 13:28:33 -0300

    Refatoração Coupon

--- database/migrations/1550864048186_coupon_schema.js
+++ database/migrations/1550864048186_coupon_schema.js
@@ -5,27 +5,22 @@ const Schema = use('Schema')
 
 class CouponSchema extends Schema {
   up() {
-    
-    this.create('coupon_type', table => {
+    this.create('coupons', (table) => {
       table.increments()
-      table.string('name', 100)
-      table.string('description', 255)
-    })
+      table.string("name, 100").notNullable()
+      table.decimal('discount', 12, 2).notNullable()
+      table.dateTime('valid_from').defaultTo(this.fn.now())
+      table.dateTime('valid_until')
+      table.integer('quantity').defaultTo(1)
 
-    this.create('coupons', table => {
-      table.increments()
-      table.decimal('discount', 12, 2)
-      table.integer('type_id').unsigned()
-
-      table.foreign('type_id')
-        .references('id')
-        .inTable('coupons')
-        .onDelete('cascade')
+      table
+        .enu('type', ['free', 'percent', 'currency'])
+        .defaultTo('currency')
+      table.boolean('recursive').defaultTo(false)
     })
   }
-  down () {
-    this.drop('coupons')
-    this.drop('coupon_type')
+  down() {
+    
   }
 }
 
